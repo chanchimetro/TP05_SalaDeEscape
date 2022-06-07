f9d98ae1ffaed4b24b35472addaaf5f71058e80e
alberinfo <43301417+alberinfo@users.noreply.github.com>
2022-06-07 09:01:07 -0300

    Add files via upload

--- Models/Escape.cs
+++ Models/Escape.cs
@@ -2,9 +2,16 @@ using System;
 using System.Collections.Generic;
 
 public static class Escape {
+    private static int _vidasRestantes;
     private static string[] _incognitasSalas;
     public static string[] _nombreViews; // -, Habitacion1, Habitacion2 ..., Victoria
     public static int _estadoJuego;
+
+    public static int VidasRestantes
+    {
+        get {return _vidasRestantes;}
+        set {_vidasRestantes = value;}
+    }
 
     public static string[] nombreViews
     {
@@ -14,6 +21,7 @@ public static class Escape {
     public static int EstadoJuego
     {
         get {return _estadoJuego;}
+        set {_estadoJuego = value;}
     }
 
     public static void InicialiazarJuego()
@@ -25,13 +33,17 @@ public static class Escape {
         _incognitasSalas[3] = "...";
         _incognitasSalas[4] = "...";
 
-        _nombreViews = new string[6];
+        _nombreViews = new string[7];
         //La sala [0] es ignorada ya que indexamos desde el 1
         _nombreViews[1] = "../Habitaciones/Habitacion1";
         _nombreViews[2] = "../Habitaciones/Habitacion2";
         _nombreViews[3] = "../Habitaciones/Habitacion3";
         _nombreViews[4] = "../Habitaciones/Habitacion4";
         _nombreViews[5] = "../Habitaciones/Victoria"; //Self explanatory
+        _nombreViews[6] = "../Habitaciones/Derrota"; //Self explanatory
+
+        _estadoJuego = 1;
+        _vidasRestantes = 3;
     }
 
     public static bool resolverSala(int sala, string incognita)
